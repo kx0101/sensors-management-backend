c24cb2363127aedfb483d90228207984cba56ac4
kx0101 <liakos.koulaxis@yahoo.com>
2024-09-11 08:35:30 +0300

    github CI workflow

--- eslint.config.js
+++ eslint.config.js
@@ -3,14 +3,14 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-    {
-        files: ["**/*.{js,mjs,cjs,ts}"],
-    },
-    {
-        languageOptions: {
-            globals: globals.browser
-        }
-    },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
+	{
+		files: ["**/*.{js,mjs,cjs,ts}"],
+	},
+	{
+		languageOptions: {
+			globals: globals.browser,
+		},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
 ];
