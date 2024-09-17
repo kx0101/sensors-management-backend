188739f1e132393893d8b8fcfd3e07c9cef593cd
kx0101 <liakos.koulaxis@yahoo.com>
2024-09-17 16:28:37 +0300

    format

--- config/alarmbell.ts
+++ config/alarmbell.ts
@@ -10,77 +10,76 @@ import { BellRepo } from "../models/bell";
  * @method testBell - Method for testing the alarm bell
  */
 export class Alarmbell extends SerialPort {
-    public status: boolean = false;
-    public disabled: boolean = false;
+	public status: boolean = false;
+	public disabled: boolean = false;
 
-    constructor() {
-        super(
-            {
-                path: "COM1",
-                baudRate: 9600,
-                autoOpen: true,
-            },
-            (err) => {
-                if (err) {
-                    logger.error("Cant find serial port");
-                }
-            },
-        );
+	constructor() {
+		super(
+			{
+				path: "COM1",
+				baudRate: 9600,
+				autoOpen: true,
+			},
+			(err) => {
+				if (err) {
+					logger.error("Cant find serial port");
+				}
+			},
+		);
 
-        this.registerListeners();
+		this.registerListeners();
 
-        if (this.status && !this.disabled) {
-            this.write("ATR1 1\n");
-            logger.info("Alarm bell is on");
-        } else {
-            this.write("ATR1 0\n");
-            logger.info("Alarm bell is off");
-        }
-    }
+		if (this.status && !this.disabled) {
+			this.write("ATR1 1\n");
+			logger.info("Alarm bell is on");
+		} else {
+			this.write("ATR1 0\n");
+			logger.info("Alarm bell is off");
+		}
+	}
 
-    private registerListeners() {
-        BellRepo.watch().on("change", async (data) => {
-            switch (data.operationType) {
-                case "insert":
-                    break;
-                case "update":
-                    this.disabled =
-                        data.updateDescription.updatedFields.status;
-                    logger.info(
-                        "Bell is ",
-                        this.disabled ? "enabled" : "disabled",
-                    );
-                    break;
-            }
-        });
+	private registerListeners() {
+		BellRepo.watch().on("change", async (data) => {
+			switch (data.operationType) {
+				case "insert":
+					break;
+				case "update":
+					this.disabled = data.updateDescription.updatedFields.status;
+					logger.info(
+						"Bell is ",
+						this.disabled ? "enabled" : "disabled",
+					);
+					break;
+			}
+		});
 
-        AlarmRepo.watch().on("change", async (data) => {
-            if (this.disabled) return;
+		AlarmRepo.watch().on("change", async (data) => {
+			if (this.disabled) return;
 
-            switch (data.operationType) {
-                case "insert":
-                    this.status = true;
-                    break;
-                case "update":
-                    if (data.updateDescription.updatedFields.aknowledged) {
-                        this.status = false;
-                    }
+			switch (data.operationType) {
+				case "insert":
+					this.status = true;
+					break;
+				case "update":
+					if (data.updateDescription.updatedFields.aknowledged) {
+						this.status = false;
+					}
 
-                    logger.log("Alarm is ", this.status ? "closed" : "open");
-                    break;
-            }
-        });
-    }
+					logger.log("Alarm is ", this.status ? "closed" : "open");
+					break;
+			}
+		});
+	}
 
-    public testBell() {
-        if (this.isOpen) {
-            logger.warn("Serial port is not open");
-            return;
-        }
-        logger.info("Testing bell...");
-        this.write("ATR1 1\n");
-        setTimeout(() => {
-            this.write("ATR1 0\n");
-        }, 5000);
-    }
+	public testBell() {
+		if (this.isOpen) {
+			logger.warn("Serial port is not open");
+			return;
+		}
+		logger.info("Testing bell...");
+		this.write("ATR1 1\n");
+		setTimeout(() => {
+			this.write("ATR1 0\n");
+		}, 5000);
+	}
 }
--- services/gatewayService.ts
+++ services/gatewayService.ts
@@ -3,33 +3,33 @@ import { logger } from "../config/logger";
 import { entriesResolvers } from "../resolver/entries/entryResolvers";
 
 export interface IData {
-    address: string;
-    type: number;
-    order: number;
-    id: number;
-    value: number;
+	address: string;
+	type: number;
+	order: number;
+	id: number;
+	value: number;
 }
 
 export const createEntry = (data: Buffer) => {
-    const parsedData = binaryParser.parse(data) as unknown as IData;
+	const parsedData = binaryParser.parse(data) as unknown as IData;
 
-    const key = JSON.stringify(parsedData);
-    const currentDate = Date.now();
+	const key = JSON.stringify(parsedData);
+	const currentDate = Date.now();
 
-    const lastAdded = sensorCache.get(key)!;
-    if (currentDate - lastAdded <= 60000) {
-        return;
-    }
+	const lastAdded = sensorCache.get(key)!;
+	if (currentDate - lastAdded <= 60000) {
+		return;
+	}
 
-    sensorCache.set(key, currentDate);
+	sensorCache.set(key, currentDate);
 
-    const entry = entriesResolvers.Mutation.createEntry(null, {
-        entryInput: {
-            address: parsedData.address,
-            sensor: parsedData.type,
-            value: parsedData.value,
-        },
-    });
+	const entry = entriesResolvers.Mutation.createEntry(null, {
+		entryInput: {
+			address: parsedData.address,
+			sensor: parsedData.type,
+			value: parsedData.value,
+		},
+	});
 
-    logger.info(entry);
+	logger.info(entry);
 };
