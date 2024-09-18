5489179475b41b45adee6482a8394561bd7b9c38
kx0101 <liakos.koulaxis@yahoo.com>
2024-09-18 15:25:35 +0300

    eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,13 @@
-// import globals from "globals";
-// import pluginJs from "@eslint/js";
-// import tsPlugin from "@typescript-eslint/eslint-plugin";
-// import tsParser from "@typescript-eslint/parser";
-//
-// export default [
-//   {
-//     files: ["**/*.{js,mjs,cjs,ts}"],  // Match JS and TS files
-//     languageOptions: {
-//       parser: tsParser,               // Use TypeScript parser
-//       globals: globals.browser,       // Use browser environment globals
-//     },
-//     plugins: {
-//       "@typescript-eslint": tsPlugin, // Include TypeScript plugin
-//     },
-//     rules: {
-//       ...pluginJs.configs.recommended.rules,   // Include recommended ESLint rules for JS
-//       ...tsPlugin.configs.recommended.rules,   // Include recommended rules for TypeScript
-//     },
-//   },
-// ];
+module.exports = [
+    {
+        rules: {
+        },
+        settings: {
+            'import/resolver': {
+                node: {
+                    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+                },
+            },
+        },
+    },
+];
