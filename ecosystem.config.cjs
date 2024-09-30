454a9a1d2fd92fb0a28fd6ea1890edbf7602bf3a
kx0101 <liakos.koulaxis@yahoo.com>
2024-09-30 07:45:36 +0300

    feat: added ecosystem config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    apps: [
+        {
+            name: 'Sensors',
+            port: '3000',
+            script: './build/index.js',
+            env: {
+                "JWT_HEADER": "jwt-secret",
+                "PORT": 3000,
+                "JWT_SECRET_KEY": "Kex2006!",
+
+                "MONGODB_URL": "mongodb://localhost:2017/",
+                "MONGODB_DBNAME": "Sensors",
+                "MONGODB_REPLICASET": "rs0",
+                "MONGODB_USERNAME": "admin",
+                "MONGODB_PASSWORD": "Kex2006!",
+
+                "GATEWAY_PORT": "10001",
+                "GATEWAY_URI": "192.168.1.10"
+            }
+        }
+    ]
+}
